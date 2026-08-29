a644ab4023aee670d26d5cf830e63a684c880cfb
Yang458 <18457376055@163.com>
2026-08-29 17:18:48 +0800

    Update d_1.gd

--- project/tscn/d_1.gd
+++ project/tscn/d_1.gd
@@ -3,8 +3,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var click_sound = preload("res://上楼梯.wav")
-	var click_suond2 = preload("res://005-Rain01.ogg")
+	var click_sound = preload("res://assets/上楼梯.wav")
+	var click_suond2 = preload("res://assets/005-Rain01.ogg")
 	SettingsManager.play_sfx(click_sound)
 	SettingsManager.play_sfx(click_suond2)
 
